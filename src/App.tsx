192bdbc1508a1ad222af903f6b70fe1c4be10c6b
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-05-03 16:00:26 +0300

    Add custom form so it can be reusable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,96 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import CustomForm from './components/CustomForm'
-import ThemeSwitch from './components/ThemeSwitch'
-
-import './assets/global.css'
+import {
+  defaultValues,
+  formSchema,
+  Interests,
+  FormValues,
+  interestOptions
+} from './utils/formSchema'
+import CustomInput from './components/CustomInput'
+import CustomSelect from './components/CustomSelect'
+import CustomFileInput from './components/CustomFileInput'
 
 const App = () => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = (values) => {
+    console.log(values)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert('Great Success!')
+        resolve(values)
+      }, 3000)
+    })
+  }
+
   return (
-    <>
-      <CustomForm />
-      <ThemeSwitch />
-    </>
+    <CustomForm onSubmit={handleSubmit(onSubmit)} isSubmitting={isSubmitting}>
+      {/* First Name */}
+      <CustomInput
+        fieldError={errors.firstName}
+        label='First Name'
+        id='firstName'
+        inputType='text'
+        register={register}
+      />
+
+      {/* Last Name */}
+      <CustomInput
+        fieldError={errors.lastName}
+        label='Last Name'
+        id='lastName'
+        inputType='text'
+        register={register}
+      />
+
+      {/* Password */}
+      <CustomInput
+        fieldError={errors.password}
+        label='Password'
+        id='password'
+        inputType='password'
+        register={register}
+      />
+
+      {/* Confirm Password */}
+      <CustomInput
+        fieldError={errors.passwordConfirmation}
+        label='Confirm Password'
+        id='passwordConfirmation'
+        inputType='password'
+        register={register}
+      />
+
+      {/* Interests */}
+      <CustomSelect<FormValues, Interests, true>
+        isMulti
+        name='interests'
+        control={control}
+        label='Interests (maximum 2)'
+        placeholder='Select some interests'
+        options={interestOptions}
+        useBasicStyles
+      />
+
+      {/* Avatar */}
+      <CustomFileInput
+        fieldError={errors.avatar}
+        label='Avatar'
+        id='avatar'
+        inputType='file'
+        register={register}
+      />
+    </CustomForm>
   )
 }
 
--- src/components/CustomForm/index.tsx
+++ src/components/CustomForm/index.tsx
@@ -1,101 +1,25 @@
-import { useForm, SubmitHandler } from 'react-hook-form'
 import { Container, Heading, Button, Stack } from '@chakra-ui/react'
-import { zodResolver } from '@hookform/resolvers/zod'
 
-import CustomSelect from '../CustomSelect'
-import CustomInput from '../CustomInput'
-import {
-  FormValues,
-  Interests,
-  defaultValues,
-  formSchema,
-  interestOptions
-} from '../../utils/formSchema'
-import CustomFileInput from '../CustomFileInput'
+import { ReactNode } from 'react'
 
-const CustomForm = () => {
-  const {
-    control,
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting }
-  } = useForm<FormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues
-  })
-  const onSubmit: SubmitHandler<FormValues> = (values) => {
-    console.log(values)
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        alert('Great Success!')
-        resolve(values)
-      }, 3000)
-    })
-  }
-
+const CustomForm = ({
+  children,
+  isSubmitting,
+  onSubmit
+}: {
+  children: ReactNode
+  isSubmitting: boolean
+  onSubmit: React.FormEventHandler<HTMLDivElement>
+}) => {
   return (
-    <Container mt={8} mb={20} as='form' onSubmit={handleSubmit(onSubmit)}>
+    <Container mt={8} mb={20} as='form' onSubmit={onSubmit}>
       {/* Title */}
       <Heading as='h1' mb={8} size='lg'>
         React Register Form
       </Heading>
-
       <Stack spacing={6}>
-        {/* First Name */}
-        <CustomInput
-          fieldError={errors.firstName}
-          label='First Name'
-          id='firstName'
-          inputType='text'
-          register={register('firstName')}
-        />
-
-        {/* Last Name */}
-        <CustomInput
-          fieldError={errors.lastName}
-          label='Last Name'
-          id='lastName'
-          inputType='text'
-          register={register('lastName')}
-        />
-
-        {/* Password */}
-        <CustomInput
-          fieldError={errors.password}
-          label='Password'
-          id='password'
-          inputType='password'
-          register={register('password')}
-        />
-
-        {/* Confirm Password */}
-        <CustomInput
-          fieldError={errors.passwordConfirmation}
-          label='Confirm Password'
-          id='passwordConfirmation'
-          inputType='password'
-          register={register('passwordConfirmation')}
-        />
-
-        {/* Interests */}
-        <CustomSelect<FormValues, Interests, true>
-          isMulti
-          name='interests'
-          control={control}
-          label='Interests (maximum 2)'
-          placeholder='Select some interests'
-          options={interestOptions}
-          useBasicStyles
-        />
-
-        {/* Avatar */}
-        <CustomFileInput
-          fieldError={errors.avatar}
-          label='Avatar'
-          id='avatar'
-          inputType='file'
-          register={register('avatar')}
-        />
+        {/* Input Elements */}
+        {children}
 
         {/* Submit Button */}
         <Button
