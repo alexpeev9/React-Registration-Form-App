d74b2457aab22e3c67c1be048d2693000cadf7f7
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-05-03 18:10:50 +0300

    Allow compare password on first step

--- src/utils/registerSchema.ts
+++ src/utils/registerSchema.ts
@@ -1,46 +1,45 @@
 import { z } from 'zod'
 import { ACCEPTED_IMAGE_TYPES, MAX_FILE_SIZE } from './constants'
 
+let password = ''
+
 const interestsSchema = z.object({
   label: z.string(),
   value: z.string()
 })
 
-export const registerSchema = z
-  .object({
-    firstName: z.string().min(1, { message: 'First name is required' }),
-    lastName: z.string().min(1, { message: 'Last name is required' }),
-    password: z
-      .string()
-      .min(6, { message: 'Password must be at least 6 characters' }),
-    passwordConfirmation: z
-      .string()
-      .min(1, { message: 'You must confirm your password' }),
-    interests: interestsSchema
-      .array()
-      .min(1, { message: 'Please pick an interest' })
-      .max(2, { message: 'Please pick maximum 2 interests' }),
-    avatar: z
-      .instanceof(FileList, { message: 'Please upload an image' })
-      .refine(
-        (files) => files[0]?.size <= MAX_FILE_SIZE,
-        `Maximum file is up to 5mb`
-      )
-      .refine(
-        (files) => ACCEPTED_IMAGE_TYPES.includes(files[0]?.type),
-        'Please upload a valid image'
-      )
-      .transform((files) => {
-        return files.item(0)!
-      })
-  })
-  .refine(
-    ({ password, passwordConfirmation }) => password === passwordConfirmation,
-    {
-      message: 'Passwords must match',
-      path: ['passwordConfirmation']
-    }
-  )
+export const registerSchema = z.object({
+  firstName: z.string().min(1, { message: 'First name is required' }),
+  lastName: z.string().min(1, { message: 'Last name is required' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters' })
+    .refine((value) => {
+      password = value
+      return true
+    }),
+  passwordConfirmation: z
+    .string()
+    .min(1, { message: 'You must confirm your password' })
+    .refine((value) => value === password, 'Password do not match'),
+  interests: interestsSchema
+    .array()
+    .min(1, { message: 'Please pick an interest' })
+    .max(2, { message: 'Please pick maximum 2 interests' }),
+  avatar: z
+    .instanceof(FileList, { message: 'Please upload an image' })
+    .refine(
+      (files) => files[0]?.size <= MAX_FILE_SIZE,
+      `Maximum file is up to 5mb`
+    )
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files[0]?.type),
+      'Please upload a valid image'
+    )
+    .transform((files) => {
+      return files.item(0)!
+    })
+})
 
 export type FormValues = z.infer<typeof registerSchema>
 
