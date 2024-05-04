06e4ca84c7b68baa26d5cb36df5d9cfac82dea47
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-05-04 14:20:52 +0300

    Optimize input fields functionality

--- src/components/CustomForm/CustomFields/index.tsx
+++ src/components/CustomForm/CustomFields/index.tsx
@@ -1,8 +1,8 @@
 import { Fragment } from 'react'
 import { FieldValues } from 'react-hook-form'
-import CustomInput from '../../CustomInput'
-import CustomSelect from '../../CustomSelect'
-import CustomFileInput from '../../CustomFileInput'
+import CustomInput from './CustomInput'
+import CustomSelect from './CustomSelect'
+import CustomFileInput from './CustomFileInput'
 import { CustomFieldsParams } from './types'
 
 const CustomFields = <T extends FieldValues, Option>({
@@ -12,44 +12,51 @@ const CustomFields = <T extends FieldValues, Option>({
   trigger,
   register
 }: CustomFieldsParams<T, Option>) => {
-  return current.content.map((index, key) => (
-    <Fragment key={key}>
-      {(index.inputType === 'text' || index.inputType === 'password') && (
-        <>
-          <CustomInput<T>
-            label={index.label}
-            name={index.name}
-            inputType={index.inputType}
-            control={control}
-            trigger={trigger}
-          />
-        </>
-      )}
-
-      {index.inputType === 'select' && index.options && (
-        <>
-          <CustomSelect<T, Option>
-            name={index.name}
-            control={control}
-            label={index.label}
-            options={index.options}
-            trigger={trigger}
-          />
-        </>
-      )}
-
-      {index.inputType === 'file' && (
-        <>
-          <CustomFileInput<T>
-            label={index.label}
-            name={index.name}
-            fieldError={errors[index.name]}
-            register={register}
-          />
-        </>
-      )}
-    </Fragment>
-  ))
+  return current.content.map((field, key) => {
+    const { inputType, label, name, options } = field
+    switch (inputType) {
+      case 'text':
+      case 'password':
+        return (
+          <Fragment key={key}>
+            <CustomInput<T>
+              label={label}
+              name={name}
+              inputType={inputType}
+              control={control}
+              trigger={trigger}
+            />
+          </Fragment>
+        )
+      case 'select':
+        return (
+          <Fragment key={key}>
+            {options && (
+              <CustomSelect<T, Option>
+                name={name}
+                control={control}
+                label={label}
+                options={options}
+                trigger={trigger}
+              />
+            )}
+          </Fragment>
+        )
+      case 'file':
+        return (
+          <Fragment key={key}>
+            <CustomFileInput<T>
+              label={label}
+              name={name}
+              fieldError={errors[name]}
+              register={register}
+            />
+          </Fragment>
+        )
+      default:
+        return null
+    }
+  })
 }
 
 export default CustomFields
